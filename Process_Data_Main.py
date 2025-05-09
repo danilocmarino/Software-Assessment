38eb3181ef9e00444999f194e6372ef29c66d616
danilocmarino <danilo.cunha.marino@hotmail.com>
2025-05-09 11:19:23 +0100

    Update Process_Data_Main.py

--- Process_Data_Main.py
+++ Process_Data_Main.py
@@ -18,17 +18,17 @@ try:
             reader = csv.reader(file) #..it craates a csv reader.
             header = next(reader) #... it reads the header.
             for row in reader:#... it makes the names working throught a row.
-                name, geography, history, portuguese = row #... subjects which needs to be calculated.
-                average = Calculation_Functions.calculate_average([int(geography), int(history), int(portuguese)]) #....it calculates the average of these subjects.
+                name, geography, history, portuguese, english, math = row #... subjects which needs to be calculated.
+                average = Calculation_Functions.calculate_average([int(geography), int(history), int(portuguese), int(english), int(math)])#calculates the average of these subjects.
                 grade = Calculation_Functions.assign_grade(average) #... knowing the average. it works giving the grade
-                Students.append([name, geography, history, portuguese, average, grade]) #... this command works using each students, average and grade.
-
+                Students.append([name, geography, history, portuguese, english, math, average, grade]) #... this command works using each students, average and grade.
+     
 except FileNotFoundError:
      print(f"Error: Students1.csv not found.") #.... this is in case of the file is not found. 
 
 with open('Student_Result.csv', 'w', newline='') as file: #...this command write everything on the result file.
      writer = csv.writer(file)
-     writer.writerow(["Name", "Geography", "History", "Portuguese", "Average", "Grade"])
+     writer.writerow(["Name", "Geography", "History", "Portuguese", "English", "Math", "Average", "Grade"])
      writer.writerows(Students)
 
 print("Results saved to student_results.csv")          
